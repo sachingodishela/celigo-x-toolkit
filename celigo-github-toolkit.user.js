f9be85de3b16c93a2467d16a87d1bcba76125666
Sachin Godishela <sachingodishela@outlook.com>
2023-04-17 13:26:56 +0530

    Fix bug when multiple JIRA ids are present

--- celigo-github-toolkit.user.js
+++ celigo-github-toolkit.user.js
@@ -5,31 +5,33 @@
 // @description  Auto create href links to jira trackers in PR title
 // @author       https://github.com/sachingodishela
 // @match        https://github.com/celigo/*/pull/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
 (function () {
-  'use strict'
+'use strict'
 
   var parent_div = document.getElementsByClassName('js-issue-title markdown-title')[0]
   var original_pr_title = parent_div.childNodes[0].textContent
-  var reg_exp = /(IO-|CS-|io-|cs-)\d+/g
-  var jira_id = original_pr_title.match(reg_exp)
-  if (!jira_id) return
-  else jira_id = jira_id[0]
-  var splitted_pr_title = original_pr_title.replaceAll(reg_exp, '@@sachin@@beaches_be_tripping@@sachin@@').split('@@sachin@@')
+  var reg_exp = /(io|cs)-\d+/gi
+  var tickets = original_pr_title.match(reg_exp).map(ticket => {
+      var a = document.createElement('a')
+      a.appendChild(document.createTextNode(ticket))
+      a.title = ticket
+      a.href = 'https://celigo.atlassian.net/browse/' + ticket
+      return a
+  })
+  var occurence = -1
+  var splitted_pr_title = original_pr_title.replaceAll(reg_exp, () => {
+      occurence = occurence + 1
+      return '_lol_$' + occurence + '_lol_'
+  }).split('_lol_').filter(a => a)
   parent_div.removeChild(parent_div.childNodes[0])
-  var aElement = document.createElement('a')
-  var text = document.createTextNode(jira_id)
-  aElement.appendChild(text)
-  aElement.title = jira_id
-  aElement.href = 'https://celigo.atlassian.net/browse/' + jira_id
   splitted_pr_title.forEach(entry => {
-    if (entry !== 'beaches_be_tripping') {
-      parent_div.appendChild(document.createTextNode(entry))
-    } else {
-      parent_div.appendChild(aElement)
-    }
+      if (!entry.startsWith('$')) {
+          parent_div.appendChild(document.createTextNode(entry))
+      } else {
+          parent_div.appendChild(tickets[Number(entry.substr(1))])
+      }
   })
 })()
